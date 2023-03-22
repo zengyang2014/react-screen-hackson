456e1e23c39ec28dce59fad9e9036c72b2bc5087
Bo.Jin <bo.jin@siemens.com>
2023-03-22 13:37:44 +0800

    Change title

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,6 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './home.scss';
 import headerBg from '../images/header.png'
-import * as echarts from 'echarts'
 import {Chart1} from "../components/chart-1";
 import {Chart2} from "../components/chart-2";
 import {Chart3} from "../components/chart-3";
@@ -17,58 +16,88 @@ import {Chart12} from "../components/chart-12";
 import {Chart13} from "../components/chart-13";
 import {Chart14} from "../components/chart-14";
 
+const formatDigits = (digit: number, length: number) => {
+  return (digit + "").padStart(length, "0")
+}
+
 export const Home = () => {
-    const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
+
+  const [timer, setTimer] = useState("")
+
+  useEffect(() => {
+    setInterval(() => {
+      const time = new Date();
+      const year = formatDigits(time.getFullYear(), 4)
+      const month = formatDigits(time.getMonth() + 1, 2);
+      const date = formatDigits(time.getDate(), 2);
+      const hour = formatDigits(time.getHours(), 2);
+      const minute = formatDigits(time.getMinutes(), 2);
+      const seconds = formatDigits(time.getSeconds(), 2);
+      const timeString = `${year}年${month}月${date} ${hour}:${minute}:${seconds}`
+      setTimer(timeString)
+    }, 1000)
+  }, [])
+
   return (
     <div className="home">
-      <header style={{backgroundImage:`url(${headerBg})`}}></header>
-        <main>
-            <section className="section1">
-                <Chart1></Chart1>
-                <Chart2></Chart2>
-            </section>
-            <section className="bordered section2">
-                <Chart3/>
-                <Chart4/>
-            </section>
-            <section className="bordered section3">
-                <Chart5/>
-            </section>
-            <section className="bordered section4">
-                <Chart6/>
-                <div className="bordered 年龄段">
-                    <h2>犯罪人员年龄段分布</h2>
-                    <div className="charts">
-                        <Chart7/>
-                        <Chart8/>
-                        <Chart9/>
-                    </div>
-                </div>
-            </section>
-            <section className="section5">
-                <div className="bordered row1 案发类型">
-                    <h2>案发类型统计</h2>
-                    <div className="charts">
-                        <Chart10/>
-                        <Chart11/>
-                    </div>
-                </div>
-                <div className="bordered row2 案发街道">
-                    <h2>案发街道统计</h2>
-                    <div className="charts">
-                        <Chart12/>
-                        <Chart13/>
-                    </div>
-                </div>
-                <div className="bordered row3 作案手段">
-                    <h2>作案手段分析</h2>
-                    <Chart14/>
-                </div>
-            </section>
-        </main>
-        <footer>
-            &copy; wanwan {year} 大屏监控
-        </footer>
+      <header style={{backgroundImage: `url(${headerBg})`}} className={"title"}>
+        <div className={"title-text"}>
+          Hackathon智能汽车制造产线
+        </div>
+        <div className={"title-timer"}>
+          <div>
+            {timer}
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className="section1">
+          <Chart1></Chart1>
+          <Chart2></Chart2>
+        </section>
+        <section className="bordered section2">
+          <Chart3/>
+          <Chart4/>
+        </section>
+        <section className="bordered section3">
+          <Chart5/>
+        </section>
+        <section className="bordered section4">
+          <Chart6/>
+          <div className="bordered 年龄段">
+            <h2>犯罪人员年龄段分布</h2>
+            <div className="charts">
+              <Chart7/>
+              <Chart8/>
+              <Chart9/>
+            </div>
+          </div>
+        </section>
+        <section className="section5">
+          <div className="bordered row1 案发类型">
+            <h2>案发类型统计</h2>
+            <div className="charts">
+              <Chart10/>
+              <Chart11/>
+            </div>
+          </div>
+          <div className="bordered row2 案发街道">
+            <h2>案发街道统计</h2>
+            <div className="charts">
+              <Chart12/>
+              <Chart13/>
+            </div>
+          </div>
+          <div className="bordered row3 作案手段">
+            <h2>作案手段分析</h2>
+            <Chart14/>
+          </div>
+        </section>
+      </main>
+      <footer>
+        &copy; wanwan {year} 大屏监控
+      </footer>
     </div>
   );
 };
