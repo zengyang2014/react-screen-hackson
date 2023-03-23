c34c9730275ce7981b49739a1b9746bd10402222
Yu Cai Hui <yucai.hui@siemens.com>
2023-03-23 20:51:26 +0800

    update the backend

--- src/backend/scada.ts
+++ src/backend/scada.ts
@@ -4,32 +4,39 @@ class ScadaCache {
   private readonly cache: {
     source: {
       material: {
-        materialA: number,
-        materialB: number,
+        materialA: number;
+        materialB: number;
         materialC: number;
       };
       product: {
         defective: number;
         qualified: number;
       };
-      faultReason: number[]
-    }
+      faultReason: { num: number; date: number }[];
+    };
   };
 
   constructor() {
+    let faultReason = [];
+    for (let i = 0; i < 9; i++) {
+      faultReason.push({
+        num: 0,
+        Date: Date.now(),
+      });
+    }
     this.cache = {
       source: {
         material: {
           materialA: 1458,
           materialB: 2564,
-          materialC: 3368
+          materialC: 3368,
         },
         product: {
           defective: 1,
           qualified: 1,
         },
-        faultReason: [0, 0, 0, 0, 0, 0, 0, 0, 0]
-      }
+        faultReason,
+      },
     };
   }
 
@@ -37,23 +44,23 @@ class ScadaCache {
     this.cache[key] = value;
   }
 
-  subMateria(){
-    this.cache.source.material.materialA -= 1
-    this.cache.source.material.materialB -= 1
-    this.cache.source.material.materialC -= 1
+  subMateria() {
+    this.cache.source.material.materialA -= 1;
+    this.cache.source.material.materialB -= 1;
+    this.cache.source.material.materialC -= 1;
   }
 
-  updateProduct(status: string){
-    if(status === '0'){
-      this.cache.source.product.qualified += 1
-    }else{
-      this.cache.source.product.defective += 1
-      this.cache.source.faultReason[Number(status) - 1] += 1
+  updateProduct(status: string) {
+    if (status === "0") {
+      this.cache.source.product.qualified += 1;
+    } else {
+      this.cache.source.product.defective += 1;
+      this.cache.source.faultReason[Number(status) - 1].num += 1;
+      this.cache.source.faultReason[Number(status) - 1].date = Date.now()
     }
   }
 
-  setPlcPortValues(data: { [key: string]: any }) {
-  }
+  setPlcPortValues(data: { [key: string]: any }) {}
 
   getCache() {
     return this.cache;
