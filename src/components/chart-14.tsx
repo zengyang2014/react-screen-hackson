8b7803a9f28d139b22aed0580aa81a7484215b3a
Yu Cai Hui <yucai.hui@siemens.com>
2023-03-23 11:14:04 +0800

    contact the web page

--- src/components/chart-14.tsx
+++ src/components/chart-14.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { useRecoilState } from 'recoil';
-import { provinceState, scadaCache } from '../state/store';
-import "./chart-14.scss"
+import React from "react";
+import { useRecoilState } from "recoil";
+import { provinceState, scadaCache } from "../state/store";
+import "./chart-14.scss";
 
 export const Chart14 = () => {
-  const [province] = useRecoilState(provinceState)
-  const [cache] = useRecoilState(scadaCache)
-  let displayNumber = []
-  if (province === 'China') {
-    for(let i =0; i < 9; i++){
-      displayNumber[i] = cache.baoding.defectiveReason[i] + cache.chengdu.defectiveReason[i] + cache.xian.defectiveReason[i] + cache.shanghai.defectiveReason[i]
+  const [province] = useRecoilState(provinceState);
+  const [cache] = useRecoilState(scadaCache);
+  let displayNumber = [];
+  if (province === "China") {
+    for (let i = 0; i < 9; i++) {
+      displayNumber[i] =
+        cache.baoding.defectiveReason[i] +
+        cache.chengdu.defectiveReason[i] +
+        cache.xian.defectiveReason[i] +
+        cache.shanghai.defectiveReason[i];
     }
-  }else{
-    displayNumber = cache.baoding.defectiveReason
+  } else {
+    displayNumber = cache.baoding.defectiveReason;
   }
   return (
     <table className={"chart-14"}>
@@ -28,7 +32,9 @@ export const Chart14 = () => {
           <td rowSpan={3}>破损</td>
           <td>机器人校准失败</td>
           <td>{displayNumber[0]}</td>
-          <td rowSpan={3}>21</td>
+          <td rowSpan={3}>
+            {displayNumber[0] + displayNumber[1] + displayNumber[2]}
+          </td>
         </tr>
         <tr>
           <td>机床加工程序错误</td>
@@ -42,7 +48,7 @@ export const Chart14 = () => {
           <td rowSpan={3}>裂缝</td>
           <td>加工温度异常</td>
           <td>{displayNumber[3]}</td>
-          <td rowSpan={3}>25</td>
+          <td rowSpan={3}>{displayNumber[3] + displayNumber[4] + displayNumber[5]}</td>
         </tr>
         <tr>
           <td>加工参数异常</td>
@@ -56,7 +62,7 @@ export const Chart14 = () => {
           <td rowSpan={3}>剐蹭</td>
           <td>机械爪过紧</td>
           <td>{displayNumber[6]}</td>
-          <td rowSpan={3}>25</td>
+          <td rowSpan={3}>{displayNumber[6] + displayNumber[7] + displayNumber[8]}</td>
         </tr>
         <tr>
           <td>传送带异物</td>
